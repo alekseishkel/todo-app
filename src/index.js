40352fd8ba9c40cd2c347ef1dcd0b0a60223d3e0
alex <lexashkel@mail.ru>
2020-05-31 17:05:34 +0300

    компоненты приложения

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
-console.log(`fdsf`);
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const TodoList = () => {
+  const items = [`Lear React`, `Build Awesome App`]
+  return (
+    <ul>
+      <li>{items[0]}</li>
+      <li>{items[1]}</li>
+    </ul>
+  );
+}
+
+const AppHeader = () => {
+  return <h1>My Todo List</h1>;
+}
+
+const SearchPanel = () => {
+  const searchText = `Type here to search`
+  return <input placeholder={searchText} />;
+}
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById(`root`));
