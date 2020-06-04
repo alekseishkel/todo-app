b85144ae95950faaa9faddf9b3a3ba65bbced09c
Alex <lexashkel@gmail.com>
2020-06-04 15:31:32 +0300

    добавление элемента пользователем

--- src/components/item-add/item-add.js
+++ src/components/item-add/item-add.js
@@ -3,13 +3,43 @@ import React, { Component } from 'react';
 import './item-add.css'
 
 export default class AddItem extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      label: ''
+    };
+
+    this.onInputChange = (evt) => {
+      this.setState({
+        label: evt.target.value
+      });
+    };
+
+    this.onSubmit = (evt) => {
+      evt.preventDefault();
+      this.props.onItemAdd(this.state.label);
+      this.setState({
+        label: ''
+      });
+    };
+  };
+
   render() {
     return (
-      <div className="item-add">
+      <form className="item-add" onSubmit={this.onSubmit}>
+        <input 
+          type='text'
+          className='form-control'
+          onChange={this.onInputChange}
+          placeholder='What needs to be done'
+          value={this.state.label}
+          />
         <button 
-          className='btn btn-outline-secondary'
-          onClick={() => this.props.onItemAdd('hello')}>Add Item</button>
-      </div>
+          className='btn btn-outline-secondary'>
+            Add Item
+        </button>
+      </form>
     )
   };
 };
